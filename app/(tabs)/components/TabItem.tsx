4c279995afd439ae390e1c5c22ab2eaf1c68324b
Luiz Karpinski <euluizkarpinski@gmail.com>
2025-04-21 20:25:55 -0300

    feat(tabs): add a white color to tabs

--- app/(tabs)/components/TabItem.tsx
+++ app/(tabs)/components/TabItem.tsx
@@ -10,18 +10,16 @@ type TabIconProps = {
 export const TabIcon = ({ focused, icon, title }: TabIconProps) => {
   if (focused) {
     return (
-      <>
-        <ImageBackground
-          className='flex flex-row w-full flex-1 min-w-[112px] min-h-[52px] mt-4 justify-center items-center rounded-full overflow-hidden gap-2'
+        <View
+          className='flex flex-row bg-white/5 w-full flex-1 min-w-[112px] min-h-[52px] mt-4 justify-center items-center rounded-full overflow-hidden gap-2'
         >
           <Image
             source={icon}
             tintColor="#c4b5fd"
             className='size-5'
           />
-          <Text className='text-violet-300 text-base'>{title}</Text>
-        </ImageBackground>
-      </>
+          <Text className='text-violet-300 text-xs'>{title}</Text>
+        </View>
     )
   } else {
     return (
